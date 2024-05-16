c7167745e8c564e37b256a96505b80fe72711ad7
mmontan0 <165187154+mmontan0@users.noreply.github.com>
2024-05-16 20:29:45 +0000

    movies details plus list

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
   get("/directors", {:controller => "directors", :action =>"index"})
+
   get("/directors/:the_id", {:controller => "directors", :action =>"show"})
-  get("/movies", {:controller => "", :action =>""})
+  
+  get("/movies", {:controller => "movies", :action =>"index"})
+
+  get("/movies/:the_id", {:controller => "movies", :action =>"show"})
+
   get("/actors", {:controller => "", :action => ""})
   get("/directors/youngest", {:controller => "", :action =>""})
+
   get("/directors/eldest", {:controller => "", :action =>""})
 end

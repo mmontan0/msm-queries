642a87ef661742526c0bb08b249a42d06afc7744
mmontan0 <165187154+mmontan0@users.noreply.github.com>
2024-05-16 20:07:55 +0000

    Adds directorshow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", {:controller => "directors", :action =>"index"})
+  get("/directors/:the_id", {:controller => "directors", :action =>"show"})
   get("/movies", {:controller => "", :action =>""})
   get("/actors", {:controller => "", :action => ""})
   get("/directors/youngest", {:controller => "", :action =>""})

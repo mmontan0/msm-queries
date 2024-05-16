4d82e54ff30b1f4feaae23b961673ccb64bcb750
mmontan0 <165187154+mmontan0@users.noreply.github.com>
2024-05-16 19:38:42 +0000

    directors id

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", {:controller => "directors", :action =>"index"})
+  get("/movies", {:controller => "", :action =>""})
+  get("/actors", {:controller => "", :action => ""})
+  get("/directors/youngest", {:controller => "", :action =>""})
+  get("/directors/eldest", {:controller => "", :action =>""})
 end
